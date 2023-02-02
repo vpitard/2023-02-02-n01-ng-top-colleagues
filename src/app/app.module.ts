0ad8afe35bf9f6cfd30e1ebaaae9e5f6888d8c05
Vincent Pitard <vpitard@diginamic-formation.fr>
2023-02-02 15:15:45 +0100

    ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+import { Component } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'ngbd-accordion-static',
+  standalone: true,
+  imports: [NgbAccordionModule],
+  templateUrl: './accordion-static.html',
+})
+export class NgbdAccordionStatic {}
